8deca8157bf38e6547f413283f5b4a2d6365c5a7
smpower <rf.wangchn@foxmail.com>
2023-10-01 17:25:03 +0800

    fix: fixed package version

--- wechat/20231002/template/src/app/page.tsx
+++ wechat/20231002/template/src/app/page.tsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Main Page</h1>
+      <h1>公众号「码上花甲」配套源码</h1>
+      <h2>Next.js: App Router - Route Group</h2>
       <Link href="/dashboard/workspace/forms">Go to `forms` page.</Link>
     </main>
   )
