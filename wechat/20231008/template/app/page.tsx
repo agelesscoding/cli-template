af55d84d6f7bad0c08950e629ddb1bf381ec3d16
smpower <rf.wangchn@foxmail.com>
2023-10-08 03:28:00 +0800

    feat: add wechat example

--- wechat/20231008/template/app/page.tsx
+++ wechat/20231008/template/app/page.tsx
@@ -7,11 +7,13 @@ export default function Page() {
   const count = useSelector(state => state.counter.value);
 
   return (
-    <div>
-      <span>{count}</span>
-      <hr />
-      <button onClick={() => dispatch(counter.actions.increment())}>increment</button>
-      <button onClick={() => dispatch(counter.actions.decrement())}>decrement</button>
+    <div style={{ maxWidth: '1200px', margin: '0 auto', textAlign: 'center' }}>
+        <p>{count}</p>
+        <button onClick={() => dispatch(counter.actions.increment())}>increment</button>
+        <button onClick={() => dispatch(counter.actions.decrement())}>decrement</button>
+        <hr />
+        <h1 style={{ textAlign: 'center', paddingTop: '80px', color: '#666' }}>「码上花甲」公众号 2023.10.09 文章配套源码</h1>
+        <img src="https://cdn.jsdelivr.net/gh/smpower/oss.hi-ruofei.com@main/wechat.png" alt="公众号「码上花甲」" />
     </div>
   )
 }
